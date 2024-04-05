277dfa40cf6f85c6b28dd0d3bc721d728c9afa8a
My VO <kathy.nguyen198@gmail.com>
2024-04-05 17:40:28 +0200

    func description

--- animal.py
+++ animal.py
@@ -1,11 +1,16 @@
 # Class definition
 class Animal(object):
-  """Makes cute animals."""
-  # For initializing our instance objects
-  def __init__(self, name, age, is_hungry):
-    self.name = name
-    self.age = age
-    self.is_hungry = is_hungry
+    """Makes cute animals."""
+    is_alive = True
+    # For initializing our instance objects
+    def __init__(self, name, age, is_hungry):
+        self.name = name
+        self.age = age
+        self.is_hungry = is_hungry
+    
+    def description(self):
+        print(self.name)
+        print(self.age)
 # Note that self is only used in the __init__()
 # function definition; we don't need to pass it
 # to our instance objects.
@@ -14,6 +19,9 @@ zebra = Animal("Jeffrey", 2, True)
 giraffe = Animal("Bruce", 1, False)
 panda = Animal("Chad", 7, True)
 
-print(zebra.name, zebra.age, zebra.is_hungry)
-print(giraffe.name, giraffe.age, giraffe.is_hungry)
-print(panda.name, panda.age, panda.is_hungry)
+print(zebra.name, zebra.age, zebra.is_hungry, zebra.is_alive)
+print(giraffe.name, giraffe.age, giraffe.is_hungry, giraffe.is_alive)
+print(panda.name, panda.age, panda.is_hungry, panda.is_alive)
+
+hippo = Animal("hippo", 2, False)
+hippo.description()
