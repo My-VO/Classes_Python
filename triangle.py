bc480eea6bb67b940073b72dfe7b5e4f7dc9d2bf
My VO <kathy.nguyen198@gmail.com>
2024-04-08 16:08:32 +0200

    check sum angles = 180

--- triangle.py
+++ triangle.py
@@ -3,3 +3,11 @@ class Triangle(object):
     self.angle1 = angle1
     self.angle2 = angle2
     self.angle3 = angle3
+    
+  number_of_sides = 3
+
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3) == 180:
+      return True
+    else:
+      return False
