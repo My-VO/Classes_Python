e36207ea2272fe7db45738ce0a5309c862fcc9b2
My VO <kathy.nguyen198@gmail.com>
2024-04-08 16:41:07 +0200

    class Equilateral

--- triangle.py
+++ triangle.py
@@ -3,7 +3,7 @@ class Triangle(object):
     self.angle1 = angle1
     self.angle2 = angle2
     self.angle3 = angle3
-    
+
   number_of_sides = 3
 
   def check_angles(self):
@@ -11,3 +11,16 @@ class Triangle(object):
       return True
     else:
       return False
+    
+my_triangle = Triangle(90, 30, 60)
+
+print (my_triangle.number_of_sides)
+print (my_triangle.check_angles())
+
+class Equilateral(Triangle):
+  angle = 60
+
+  def __init__(self):
+    self.angle1 = self.angle
+    self.angle2 = self.angle
+    self.angle3 = self.angle
