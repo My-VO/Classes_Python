d69cc277ac78f3e6bf3743a246bf798906ea54a9
My VO <kathy.nguyen198@gmail.com>
2024-04-09 11:20:17 +0200

    ElectricCar

--- car.py
+++ car.py
@@ -3,7 +3,31 @@ class Car(object):
   def __init__(self, model, color, mpg):
     self.model = model
     self.color = color
-    self.mpg = mpg
+    self.mpg   = mpg
 
-my_car = Car("DeLorean", "silver", 88)
+  def display_car(self):
+    return "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
+
+  def drive_car(self):
+    self.condition = 'used'
+
+your_car = Car("DeLorean", "silver", 88)
+
+print (your_car.condition)
+your_car.drive_car()
+print (your_car.condition)
+
+class ElectricCar(Car):
+  def __init__(self, model, color, mpg, battery_type):
+    self.model = model
+    self.color = color
+    self.mpg   = mpg
+    self.battery_type = battery_type
+
+  def drive_car(self):
+    self.condition = "like new"
+
+my_car = ElectricCar("Tesla", "white", 100, "molten salt")
+print (my_car.condition)
+my_car.drive_car()
 print (my_car.condition)
